dac54a776d71098ec7b6e1c5c6d8af74ad08ff83
Hicaro Andre <hicaroandre@hotmail.com>
2025-07-02 23:17:14 -0300

    feat: ligação with Front more Cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // 👈 IMPORTANTE
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger.json');
 const connectDB = require('./config/db');
@@ -7,11 +8,22 @@ const routes = require('./routes/routes');
 
 const app = express();
 
+// Conecta com o banco de dados
 connectDB();
 
+// Middlewares
+app.use(cors()); // 👈 Habilita CORS para requisições do front-end
 app.use(express.json());
 
+// Rota para documentação Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// Rota base para teste com o front-end
+app.get('/', (req, res) => {
+  res.json({ message: 'API conectada com sucesso ao front-end!' });
+});
+
+// Demais rotas da aplicação
 app.use(routes);
 
 module.exports = app;
--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,6 @@
+// index.js
 const app = require('./app');
 
-
-app.listen(3333, () => console.log("Server is running"))
+app.listen(3333, () => {
+  console.log('✅ Servidor rodando em http://localhost:3333');
+});
