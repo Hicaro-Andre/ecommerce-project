4bc87b7c736f447e2c2caf699358fb632fca0aac
Hicaro Andre <hicaroandre@hotmail.com>
2025-07-17 17:09:20 -0300

    feat: start of to change confg of js for ts

---
+++ api/src/app.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config()
+
+import express, { Request, Response } from "express";
+const app = express()
+
+import connectDB from "./config/db";
+connectDB();
+
+import routes from "./routes/routes";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
+
+app.use(cors());
+app.use(express.json());
+
+app.use("./api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Servidor está rodando")
+});
+
+app.use(routes);
+
+
+export default app;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,7 @@
-
-const app = require('./app');
+import app from "./app"
 
 //apenas inicia o servidor
 app.listen(3333, () => {
   console.log('✅ Servidor rodando em http://localhost:3333');
 });
 
-//rota raiz (teste)
-app.get("/", (req, res) => {
-  res.send("Servidor está rodando");
-});
